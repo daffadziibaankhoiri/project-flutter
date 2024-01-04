61f54265b35644c18eed0096d9ed0715d754fd10
daffadziibaankhoiri <daffa.dziibaan.khoiri@gmail.com>
2024-01-04 13:14:53 +0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+// import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: const Center(
+          child: Text(
+            "My First Flutter Project",
+            overflow: TextOverflow.fade,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 5,
+              decorationStyle: TextDecorationStyle.solid,
+              decoration: TextDecoration.lineThrough,
+              decorationColor: Colors.red,
+              decorationThickness: 2,
+            ),
+
+          ),
+        ),
+        appBar: AppBar(
+
+          backgroundColor: Colors.blueAccent,
+          centerTitle: true,
+          title: const Text(
+              "Flutter First App",
+              style: TextStyle(
+              color: Colors.white,
+                fontWeight: FontWeight.bold
+          ),
+          ),
+
+          ),
+      ),
+    );
+  }
+}
+
